760d726ba25fd39de92abf24467fb54b0517ba7b
P-i-N <Pinter.Jan@gmail.com>
2016-08-06 13:54:03 +0200

    Working version

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,14 @@
+newoption { trigger = "cwd", value = "path", description = "Working directory for solution setup" }
+
+-- Remember CWD
+local oldCWD = os.getcwd()
+
+if _OPTIONS["cwd"] then
+  os.chdir(_OPTIONS["cwd"])
+else
+  _OPTIONS["cwd"] = oldCWD
+end
+
 -- Set default globals
 SolutionName = ""
 IncludeDirs = { ".", "src" }
@@ -6,5 +17,12 @@ LibDirs = { }
 -- Get solution setup
 dofile("solution.lua")
 
--- Run main Premaker script
-dofile("premaker.lua")
+-- If Premaker checkout exists in parent directory, use it instead, otherwise try to use CWD
+if os.isfile("../Premaker/premaker.lua") then
+  dofile("../Premaker/premaker.lua")
+else
+  dofile("premaker.lua")
+end
+
+-- Restore old working directory
+os.chdir(oldCWD)
