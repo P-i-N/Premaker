3fb627d0f28f45d8e62b0dab4a3a6028777be5e0
P-i-N <Pinter.Jan@gmail.com>
2016-08-06 14:06:04 +0200

    premaker.bat

---
+++ premaker.bat
@@ -0,0 +1,26 @@
+@echo off
+
+if [%1]==[] goto autodetect
+
+premake5 %1
+goto done
+
+:autodetect
+set GENERATOR=""
+
+rem Detect Visual Studio 2015
+rem -------------------------
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin" (
+set GENERATOR="vs2015"
+echo Visual Studio 2015 detected!
+) else (
+set GENERATOR=%GENERATOR%
+)
+
+if %GENERATOR%=="" (
+echo Visual Studio not detected! You must specify a premake generator manually.
+) else (
+premake5 --file=../Premaker/premake5.lua vs2015 --cwd=%cd% %GENERATOR%
+)
+
+:done
